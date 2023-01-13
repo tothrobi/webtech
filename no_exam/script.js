fefe2d6a1638f6ff23a2b3b56567ffcbc23a7790
tothrobi <toth.robert9510@gmail.com>
2023-01-13 11:35:03 +0100

    valami

--- no_exam/script.js
+++ no_exam/script.js
@@ -1,3 +1,9 @@
+class Alkatresz {
+  constructor(name, count) {
+    this.name = name;
+    this.count = count;
+  }
+}
 const form = document.querySelector("form");
 const table = document.querySelector("table tbody");
 
@@ -5,10 +11,14 @@ form.addEventListener("submit", event => {
   event.preventDefault();
   const name = document.querySelector("#partname").value;
   const count = document.querySelector("#partcount").value;
+  const part = new Alkatresz(name, count);
 
   const newRow = table.insertRow();
   const nameCell = newRow.insertCell(0);
   const countCell = newRow.insertCell(1);
-  nameCell.innerHTML = name;
-  countCell.innerHTML = count;
+  nameCell.innerHTML = part.name;
+  countCell.innerHTML = part.count;
+
+  const totalCount = table.getElementsByTagName("tr").length -1;
+  alert("Összesen " + totalCount + " alkatrészünk van!");
 });
